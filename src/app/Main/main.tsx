dd8dc4bcafbd9585ad92dda773e7d7d3670b5957
boburastonov <boburastonov33@gmail.com>
2024-08-18 14:34:34 +0500

    add footer section

--- src/app/Main/main.tsx
+++ src/app/Main/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Sidebar from "../components/sidebar";
 import Header from "../components/header";
+import Footer from "../components/footer";
 
 const Main = () => {
   return (
     <section className="h-[100vh]">
       <Header />
       <Sidebar />
+      <Footer />
     </section>
   );
 };
---
+++ src/app/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='w-[85.4%] py-4 bg-white text-[#3e3e3e] fixed bottom-0 right-0'>
+      <p className='text-center'>© 2024. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
