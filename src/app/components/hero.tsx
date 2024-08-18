77c848d466e77bfd0113a205b8f6ca06ae8a6fa5
boburastonov <boburastonov33@gmail.com>
2024-08-18 15:06:04 +0500

    add hero component type

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Hero = () => {
-  return <main className="w-[85.4%] flex-1 p-[30px] ml-auto bg-[#4094f726]"></main>;
+interface HeroProps {
+  children: React.ReactNode; // Add this line to accept children
+}
+
+const Hero: React.FC<HeroProps> = () => {
+  return (
+    <main className="w-[85.4%] flex-1 p-[30px] ml-auto bg-[#4094f726]"></main>
+  );
 };
 
 export default Hero;
