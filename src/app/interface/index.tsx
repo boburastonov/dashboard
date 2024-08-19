4ab3cd51fd968a06051839e1d0b93b56547d41c5
boburastonov <boburastonov33@gmail.com>
2024-08-19 16:37:45 +0500

    add global interface for getting datas from axios

--- src/app/interface/index.tsx
+++ src/app/interface/index.tsx
@@ -4,3 +4,17 @@ export interface sideBar {
   icon: JSX.Element;
   path: string;
 }
+
+export interface MyResponseData {
+  phone: number;
+  password: string;
+  success: boolean;
+  message: string;
+  data: {
+    tokens: {
+      accessToken: {
+        token: string;
+      };
+    };
+  };
+}
