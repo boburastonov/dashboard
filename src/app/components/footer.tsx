3a3726a3f75df4bbe9e832351ee44a63d77dba8c
boburastonov <boburastonov33@gmail.com>
2024-08-19 18:28:41 +0500

    add border-top to footer section

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-[85.4%] py-4 bg-white text-[#3e3e3e] fixed bottom-0 right-0">
+    <footer className="w-[85.4%] py-4 bg-white text-[#3e3e3e] fixed bottom-0 right-0 border-t border-solid">
       <p className="text-center">© 2024. All rights reserved.</p>
     </footer>
   );
