7a65b95a944a39098781803576121ab3ded065df
boburastonov <boburastonov33@gmail.com>
2024-08-19 16:36:01 +0500

    add components to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import Main from "./Main/main";
+"use client";
+import { useRouter } from "next/navigation";
+import Login from "./login/page";
+import Main from "./main/page";
+import { ToastContainer } from "react-toastify";
 
 export default function Home() {
+  let router = useRouter();
+  const goGome = () => {
+    router.push("/");
+  };
+  const goLogin = () => {
+    router.push("/login");
+  };
+
+  let tokenPr = localStorage.getItem("token");
+  if (tokenPr?.includes("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1")) {
+    goGome();
+  } else {
+    goLogin();
+  }
   return (
     <>
       <Main />
+      <Login />
+      <ToastContainer />
     </>
   );
 }
