cf37e74b5a5b184edd0b97591b615861a32e9b83
boburastonov <boburastonov33@gmail.com>
2024-08-24 11:42:05 +0500

    add filter functions to categories section

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -7,10 +7,11 @@ import Sidebar from "../components/sidebar";
 import Footer from "../components/footer";
 import Image from "next/image";
 import { toast } from "react-toastify";
+import { IoSearchSharp } from "react-icons/io5";
 
 const Categories: React.FC = () => {
   const [open, setOpen] = useState<boolean>(true);
-
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const [openModal, setOpenModal] = useState(false);
   const [editModal, setEditModal] = useState<boolean>(false);
   const [data, setData] = useState<MyResponseData[] | undefined>();
@@ -25,6 +26,7 @@ const Categories: React.FC = () => {
   formData.append("name_en", nameEn || "");
   formData.append("images", pic || "");
 
+  //add categories modal open function
   const openModalItem = () => {
     setOpenModal(true);
     setEditModal(false);
@@ -33,6 +35,8 @@ const Categories: React.FC = () => {
     setOpenModal(false);
     setEditModal(false); // Edit modalni yopish
   };
+
+  //close two edit and add modal items
   const editModalFunction = (status: boolean, item?: MyResponseData) => {
     setOpenModal(false);
     setEditModal(true);
@@ -57,6 +61,7 @@ const Categories: React.FC = () => {
     };
   }, [openModal, editModal]);
 
+  //get datas from backend
   const getFunction = () => {
     axios
       .get("https://autoapi.dezinfeksiyatashkent.uz/api/categories")
@@ -68,6 +73,7 @@ const Categories: React.FC = () => {
     getFunction();
   }, []);
 
+  //add new datas function
   const addFunction = (e: React.FormEvent) => {
     e.preventDefault(); // Form submitlashi oldini olish uchun
     axios
@@ -137,6 +143,14 @@ const Categories: React.FC = () => {
       })
       .catch((err) => console.error("API request failed: ", err));
   };
+
+  //filter items
+  //filter items
+  const filteredData = (data || []).filter(
+    (item) =>
+      item.name_ru?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      item.name_en?.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <section className="h-[100vh]">
@@ -338,25 +352,45 @@ const Categories: React.FC = () => {
             open ? "w-[84.4%]" : "w-[93.8%]"
           } fixed top-[60px] bottom-[57px] right-0 p-[30px] ml-auto bg-[#4094f726] transition-all duration-[0.3s]`}
         >
+          <form className="w-[50%]">
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <IoSearchSharp className="w-5 h-5 text-gray-500 dark:text-gray-400" />
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search Mockups, Logos..."
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </div>
+          </form>
           <table className="min-w-full border-collapse block md:table">
             <thead className="block md:table-header-group">
-              <tr className="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
-                <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-center block md:table-cell">
+              <tr className="border md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
+                <th className="bg-gray-700 p-2 text-white font-bold md:border md:border-grey-500 text-center block md:table-cell">
                   &#8470;
                 </th>
-                <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-center block md:table-cell">
+                <th className="bg-gray-700 p-2 text-white font-bold md:border md:border-grey-500 text-center block md:table-cell">
                   Name Ru
                 </th>
-                <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-center block md:table-cell">
+                <th className="bg-gray-700 p-2 text-white font-bold md:border md:border-grey-500 text-center block md:table-cell">
                   NameEn
                 </th>
-                <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-center block md:table-cell">
+                <th className="bg-gray-700 p-2 text-white font-bold md:border md:border-grey-500 text-center block md:table-cell">
                   Image
                 </th>
-                <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-center block md:table-cell">
+                <th className="bg-gray-700 p-2 text-white font-bold md:border md:border-grey-500 text-center block md:table-cell">
                   Actions
                 </th>
-                <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-center block md:table-cell">
+                <th className="bg-gray-700 p-2 text-white font-bold md:border md:border-grey-500 text-center block md:table-cell">
                   <button
                     onClick={openModalItem}
                     className="bg-[#1677ff] hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
@@ -367,9 +401,9 @@ const Categories: React.FC = () => {
               </tr>
             </thead>
             <tbody className="block md:table-row-group">
-              {data &&
-                Array.isArray(data) &&
-                data.map((item, index) => (
+              {filteredData &&
+                Array.isArray(filteredData) &&
+                filteredData.map((item, index) => (
                   <tr
                     key={item?.id}
                     className="bg-gray-300 border border-grey-500 md:border-none block md:table-row"
